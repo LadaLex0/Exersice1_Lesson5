049e4e1d82957b0410c3496064bda23fe5eff485
LadaLex0 <91010364+LadaLex0@users.noreply.github.com>
2021-10-04 15:28:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-n = 2
-flag = True
-
-while n < 100:
-    i = 2
-    while i < n:
-        if n % i == 0:
-            flag = False
-            break
-        else:
-            flag = True
-        i = i + 1
-    if flag:
-        print(n)
-    n = n + 1
+i=1
+while i<=100:
+    if i%7==0: print(i)
+    i=i+1
